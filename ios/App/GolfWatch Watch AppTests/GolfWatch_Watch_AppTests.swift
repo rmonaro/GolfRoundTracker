793116afc2ebb289787c59842817fdf800ec6b53
Roy Monaro <rmonaro03@gmail.com>
2026-05-29 09:22:56 -0400

    Updated

---
+++ ios/App/GolfWatch Watch AppTests/GolfWatch_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GolfWatch_Watch_AppTests.swift
+//  GolfWatch Watch AppTests
+//
+//  Created by Roy Monaro on 5/28/26.
+//
+
+import Testing
+@testable import GolfWatch_Watch_App
+
+struct GolfWatch_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
